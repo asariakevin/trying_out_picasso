9dc9bd1f0bcfbbf0de21465ac0d668d1f05fe00a
asariakevin <asariakevin@gmail.com>
2019-05-15 11:32:50 +0300

    Load NASA image

---
+++ app/src/main/java/com/example/playingwithpicasso/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.playingwithpicasso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Picasso.get().load("https://apod.nasa.gov/apod/image/1905/NGC4921_HubbleShatz_960.jpg")
+            .resize(200,200)
+            .into(imageView)
+
+    }
+}
